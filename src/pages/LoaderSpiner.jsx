e88cde9a323c5e061c7e96b11a174745aab928b3
Vivek Kushwaha <codevivekk@gmail.com>
2024-12-17 18:19:35 +0530

    push

--- src/pages/LoaderSpiner.jsx
+++ src/pages/LoaderSpiner.jsx
@@ -9,11 +9,21 @@ const LoaderSpiner = () => {
       duration: 1,
       ease: "linear",
     });
+    gsap.to(".pulse", {
+      scale: 1.5,
+      yoyo: true,
+      repeat: -1,
+      duration: 1,
+      ease: "linear",
+    });
   }, []);
   return (
     <div className="">
       <h1>Loader Spinner</h1>
-      <div className="spinner h-[400px] w-[400px] border-t-4 border-red-400 m-auto mt-10 rounded-full "></div>
+      <div className="wrappper flex">
+        <div className="spinner h-[400px] w-[400px] border-t-4 border-red-400 m-auto mt-10 rounded-full "></div>
+        <div className="pulse h-[200px]  w-[200px] border-4 border-red-400 m-auto mt-28 rounded-full "></div>
+      </div>
     </div>
   );
 };
